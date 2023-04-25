4f458ddbb0673a5b782b50427c152d74387bc121
Vitor Pereira <vitormahub@gmail.com>
2023-04-25 18:32:27 -0300

    Adicionando projeto Front-end

---
+++ web-app/app-web/src/components/MainMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card>
+      <v-navigation-drawer
+        theme="dark"
+        rail
+        permanent
+      >
+
+        <v-divider></v-divider>
+
+        <v-list
+          density="compact"
+          nav
+        >
+          <v-list-item prepend-icon="mdi-view-dashboard" value="dashboard"></v-list-item>
+
+          <v-list-item prepend-icon="mdi-forum" value="messages"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-navigation-drawer permanent>
+        <v-list>
+          <v-list-item title="Home" value="home"></v-list-item>
+
+          <v-list-item title="Contacts" value="contacts"></v-list-item>
+
+          <v-list-item title="Settings" value="settings"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+ 
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'MainMenu',
+}
+
+</script>
